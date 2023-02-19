0be37af6f3a50ae3802053bddc40260423425857
CyrilleKolle <ngidekollecyrille@gmail.com>
2023-02-19 17:25:08 +0100

    added dash plotly, setup

--- main.py
+++ main.py
@@ -20,11 +20,11 @@ df = cars_data.cars_dataframe("used")
 
 app = dash.Dash(
     __name__,
-    external_stylesheets=[dbc.themes.BOOTSTRAP],
+    external_stylesheets=[dbc.themes.DARKLY],
     meta_tags=[dict(name="viewport", content="width=device-width, initial-scale=1.0")],
 )
-app.layout = Layout(symbol_dict).layout()
 
+app.layout = Layout(symbol_dict).layout()
 
 @app.callback(
     Output("cleaned-data", "data"),
@@ -32,7 +32,6 @@ app.layout = Layout(symbol_dict).layout()
 )
 def dataframe(car):
     df_used_cars = df[0]
-    print(df_used_cars)
     return df_used_cars.to_json()
 
 
@@ -48,8 +47,7 @@ def fuel_types(df):
         .reset_index()
         .sort_values(by="model", ascending=False)
     )
-    return px.pie(df, names="fuel", values="model", title="Fuel distribution among used cars in Sweden")
-
+    return px.pie(df, names="fuel", values="model", title="Fuel distribution among used cars in Sweden", hole=0.4, facet_col_spacing=0.6)
 
 @app.callback(
     Output("top-dealers", "figure"),
@@ -76,22 +74,36 @@ def yearly_car_emmission(df, slider_value):
     dff = dff.groupby(['entry_year']).agg({'co2_emission_g/km':'sum', 'model':'count'}).reset_index()
     dff = dff[(dff['entry_year'] >= slider_value[0]) & (dff['entry_year'] <= slider_value[1])]
 
-    trace1 = px.bar(dff, x= "entry_year",
-                    y='co2_emission_g/km')
-    trace2 = px.bar(dff, x= "entry_year",
-                    y='model')
+    trace1 = px.line(dff, x= "entry_year",
+                    y='co2_emission_g/km', log_y=True)
+    trace2 = px.line(dff, x= "entry_year",
+                    y='model', log_y=True)
     
     trace_list = [trace1, trace2]
     y_axis_titles = ["Co2 emission", "total amout of car registered"]
 
     fig = make_subplots(rows=1, cols=1)
+    
+    lines = [{'dash': 'solid', 'color': 'green'},
+            {'dash': 'dot', 'color': 'red'}]
         
     for i, (item, title) in enumerate(zip(trace_list, y_axis_titles)):
-        fig.add_trace(go.Bar(name=title,
+        fig.add_trace(go.Scatter(line=lines[i] ,name=title,
                                 x=item.data[0]['x'], y=item.data[0]['y'], showlegend=True), row=1, col=1)
     fig.update_layout(title="Various countries`s individual medal", plot_bgcolor="rgba(255,255,255,0.9)")
     fig.update_xaxes(title="Medals")
     fig.update_yaxes(title_text="y-axis in logarithmic scale", type="log")
     return fig
+
+@app.callback(
+    Output('manufacturers', 'figure'),
+    Input('cleaned-data', 'data')
+)
+def manufacturer_distribution(df):
+    dff = pd.read_json(df)
+    dff['co2_emission_g/km'].fillna(0, inplace=True, downcast='infer')
+    dff = dff.groupby(['manufacturer']).agg({'model':'count', 'co2_emission_g/km': 'sum'}).reset_index()
+    
+    return px.histogram(df, x="manufacturer", y='model', color="sex")
 if __name__ == "__main__":
     app.run(debug=True)
